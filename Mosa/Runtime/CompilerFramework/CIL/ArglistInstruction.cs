67a743ddf3d19de8c42e70255e482ac527ef353c
Simon Wollwage <kintaro@think-in-co.de>
2009-12-06 14:27:10 +0100

    Cleaned some code

--- Mosa/Runtime/CompilerFramework/CIL/ArglistInstruction.cs
+++ Mosa/Runtime/CompilerFramework/CIL/ArglistInstruction.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * (c) 2008 MOSA - The Managed Operating System Alliance
  *
  * Licensed under the terms of the New BSD License.
@@ -14,50 +14,49 @@ using System.Text;
 
 namespace Mosa.Runtime.CompilerFramework.CIL
 {
-	/// <summary>
-	/// 
-	/// </summary>
-	public sealed class ArglistInstruction : BaseInstruction
-	{
-		#region Construction
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class ArglistInstruction : BaseInstruction
+    {
+        #region Construction
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="ArglistInstruction"/> class.
-		/// </summary>
-		/// <param name="opcode">The opcode.</param>
-		public ArglistInstruction(OpCode opcode)
-			: base(opcode)
-		{
-		}
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArglistInstruction"/> class.
+        /// </summary>
+        /// <param name="opcode">The opcode.</param>
+        public ArglistInstruction (OpCode opcode) : base(opcode)
+        {
+        }
 
-		#endregion // Construction
+        #endregion
 
-		#region Methods
+        #region Methods
 
-		/// <summary>
-		/// Allows visitor based dispatch for this instruction object.
-		/// </summary>
-		/// <param name="visitor">The visitor.</param>
-		/// <param name="context">The context.</param>
-		public override void Visit(ICILVisitor visitor, Context context)
-		{
-			visitor.Arglist(context);
-		}
+        /// <summary>
+        /// Allows visitor based dispatch for this instruction object.
+        /// </summary>
+        /// <param name="visitor">The visitor.</param>
+        /// <param name="context">The context.</param>
+        public override void Visit (ICILVisitor visitor, Context context)
+        {
+            visitor.Arglist (context);
+        }
 
-		/// <summary>
-		/// Decodes the specified instruction.
-		/// </summary>
-		/// <param name="ctx">The context.</param>
-		/// <param name="decoder">The instruction decoder, which holds the code stream.</param>
-		public override void Decode(Context ctx, IInstructionDecoder decoder)
-		{
-			// Decode base classes first
-			base.Decode(ctx, decoder);
+        /// <summary>
+        /// Decodes the specified instruction.
+        /// </summary>
+        /// <param name="ctx">The context.</param>
+        /// <param name="decoder">The instruction decoder, which holds the code stream.</param>
+        public override void Decode (Context ctx, IInstructionDecoder decoder)
+        {
+            // Decode base classes first
+            base.Decode (ctx, decoder);
 
-			throw new NotImplementedException();
-		}
+            throw new NotImplementedException ();
+        }
 
-		#endregion Methods
+        #endregion Methods
 
-	}
+    }
 }
--- Mosa/Runtime/CompilerFramework/CIL/DupInstruction.cs
+++ Mosa/Runtime/CompilerFramework/CIL/DupInstruction.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * (c) 2008 MOSA - The Managed Operating System Alliance
  *
  * Licensed under the terms of the New BSD License.
@@ -14,49 +14,48 @@ using System.Text;
 
 namespace Mosa.Runtime.CompilerFramework.CIL
 {
-	/// <summary>
-	/// 
-	/// </summary>
-	public sealed class DupInstruction : UnaryInstruction
-	{
-		#region Construction
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class DupInstruction : UnaryInstruction
+    {
+        #region Construction
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="NopInstruction"/> class.
-		/// </summary>
-		public DupInstruction(OpCode opcode)
-			: base(opcode,2)
-		{
-		}
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NopInstruction"/> class.
+        /// </summary>
+        public DupInstruction (OpCode opcode) : base(opcode, 2)
+        {
+        }
 
-		#endregion // Construction
+        #endregion
 
-		#region Methods
+        #region Methods
 
-		/// <summary>
-		/// Validates the specified instruction.
-		/// </summary>
-		/// <param name="ctx">The context.</param>
-		/// <param name="compiler">The compiler.</param>
-		public override void Validate(Context ctx, IMethodCompiler compiler)
-		{
-			base.Validate(ctx, compiler);
+        /// <summary>
+        /// Validates the specified instruction.
+        /// </summary>
+        /// <param name="ctx">The context.</param>
+        /// <param name="compiler">The compiler.</param>
+        public override void Validate (Context ctx, IMethodCompiler compiler)
+        {
+            base.Validate (ctx, compiler);
 
-			ctx.Result = ctx.Operand1;
-			ctx.Result2 = ctx.Operand1;
-		}
+            ctx.Result = ctx.Operand1;
+            ctx.Result2 = ctx.Operand1;
+        }
 
-		/// <summary>
-		/// Allows visitor based dispatch for this instruction object.
-		/// </summary>
-		/// <param name="visitor">The visitor.</param>
-		/// <param name="context">The context.</param>
-		public override void Visit(ICILVisitor visitor, Context context)
-		{
-			visitor.Dup(context);
-		}
+        /// <summary>
+        /// Allows visitor based dispatch for this instruction object.
+        /// </summary>
+        /// <param name="visitor">The visitor.</param>
+        /// <param name="context">The context.</param>
+        public override void Visit (ICILVisitor visitor, Context context)
+        {
+            visitor.Dup (context);
+        }
 
-		#endregion Methods
+        #endregion Methods
 
-	}
+    }
 }
