f13960ed2e1cf96278f976bcefb8442a51ff948d
Simon Wollwage <kintaro@think-in-co.de>
2009-12-06 14:24:19 +0100

    Cleaned AddInstruction

--- Mosa/Runtime/CompilerFramework/CIL/AddInstruction.cs
+++ Mosa/Runtime/CompilerFramework/CIL/AddInstruction.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * (c) 2008 MOSA - The Managed Operating System Alliance
  *
  * Licensed under the terms of the New BSD License.
@@ -14,36 +14,35 @@ using System.Text;
 
 namespace Mosa.Runtime.CompilerFramework.CIL
 {
-	/// <summary>
-	/// 
-	/// </summary>
-	public sealed class AddInstruction : ArithmeticInstruction
-	{
-		#region Construction
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class AddInstruction : ArithmeticInstruction
+    {
+        #region Construction
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="AddInstruction"/> class.
-		/// </summary>
-		public AddInstruction(OpCode opCode)
-			: base(opCode)
-		{
-		}
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AddInstruction"/> class.
+        /// </summary>
+        public AddInstruction (OpCode opCode) : base(opCode)
+        {
+        }
 
-		#endregion // Construction
+        #endregion
 
-		#region Methods
+        #region Methods
 
-		/// <summary>
-		/// Allows visitor based dispatch for this instruction object.
-		/// </summary>
-		/// <param name="visitor">The visitor.</param>
-		/// <param name="context">The context.</param>
-		public override void Visit(ICILVisitor visitor, Context context)
-		{
-			visitor.Add(context);
-		}
+        /// <summary>
+        /// Allows visitor based dispatch for this instruction object.
+        /// </summary>
+        /// <param name="visitor">The visitor.</param>
+        /// <param name="context">The context.</param>
+        public override void Visit (ICILVisitor visitor, Context context)
+        {
+            visitor.Add (context);
+        }
 
-		#endregion Methods
+        #endregion Methods
 
-	}
+    }
 }
