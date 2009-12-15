6b935143b83dcbf49e3771806de37bea89996df4
Simon Wollwage <kintaro@think-in-co.de>
2009-12-15 10:58:34 +0100

    Fixed duplicate Main method problem

--- Mosa/Emulator/HelloWorld/Boot.cs
+++ Mosa/Emulator/HelloWorld/Boot.cs
@@ -17,7 +17,7 @@ namespace Mosa.HelloWorld
 		/// <summary>
 		/// Mains this instance.
 		/// </summary>
-		public static void Main()
+		public static void BootMain()
 		{
 			Screen.Clear();
 			Screen.Color = 0x0E;
